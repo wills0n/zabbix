8df441fec1a92dbfd3e3ac951869135e0a0477af
wills0n <p140kp102rus@gmail.com>
2020-05-06 22:14:46 +0300

    added instance, provider, vpc, last work version terraform 0.12.24

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+resource "google_compute_firewall" "firewal_vpn" {
+  name = "allow-zabbix"
+
+  # name of net
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = [
+      "443", "80", "10050", "10051"
+    ]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["${var.zabbix_tag}"]
+}
